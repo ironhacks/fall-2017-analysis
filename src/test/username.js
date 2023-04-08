d27c09b9b6f0c48217666ba7ce10e47baa4ac993
jialincheoh <jcheoh@purdue.edu>
2023-04-08 11:07:17 -0400

    remove source code for visual

--- src/test/username.js
+++
@@ -1,5 +0,0 @@
-const util = require("../util");
-
-const urls = require("../lists/GoldIronHack-Phase1");
-
-console.log(urls.map(c => util.getUsernameLink(c, true)));
--- src/tools/remove-white-area.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const util = require("../util");
-
-util.removeWhiteArea(process.argv[2]);
